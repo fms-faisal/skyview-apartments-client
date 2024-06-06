b623b634f6711c709d9f28a5c917e468f560c741
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-07 04:46:28 +0600

    layout and outlet setup

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
+    children: [
+        {
+            path: "/",
+        }
+    ]
   },
 ]);
