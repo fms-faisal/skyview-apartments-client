af9a70d69e2cf60fd2c7290dc5f8eb89ebca59ab
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-08 03:56:50 +0600

    helmet added in login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const Login = () => {
     return (
         <div>
+            <div>
+                <Helmet>
+                    <title> Skyview Apartments | Login
+                    </title>
+                </Helmet>
+            </div>
             <h1>Login</h1>
         </div>
     );
