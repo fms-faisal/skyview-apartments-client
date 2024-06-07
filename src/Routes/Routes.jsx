ed1c5cdb544501c6421e5298027e2b80258d1812
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-08 03:48:50 +0600

    Login component created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
 import Apartment from "../Pages/Apartment/Apartment";
+import Login from "../Pages/Login/Login";
 
 export const router = createBrowserRouter([
   {
@@ -16,6 +17,10 @@ export const router = createBrowserRouter([
         path: "/apartment",
         element: <Apartment></Apartment>,
       },
+      {
+        path: "/login",
+        element: <Login></Login>
+      }
     ],
   },
 ]);
