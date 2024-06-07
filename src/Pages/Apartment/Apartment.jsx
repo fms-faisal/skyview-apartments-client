238886fcb1d0c7581ba0825d7dae025e1d0b5861
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-07 07:24:14 +0600

    react helmet integration

--- src/Pages/Apartment/Apartment.jsx
+++ src/Pages/Apartment/Apartment.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { Helmet } from "react-helmet-async";
 const Apartment = () => {
     return (
         <div>
-            
+            <Helmet>
+                <title>Skyview Apartments | Apartment</title>
+            </Helmet>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
+import { Helmet } from 'react-helmet-async';
 
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Skyview Apartments | Home</title>
+            </Helmet>
            <Banner></Banner>
         </div>
     );
