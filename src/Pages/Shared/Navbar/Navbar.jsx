c914a120fcc7f0099b9e6c925b86d6194ded77f4
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-08 03:54:16 +0600

    avatar added in navbar with login

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from './../../../../bannerImages/logo.jpeg'
+import logo from "./../../../../bannerImages/logo.jpeg";
 const Navbar = () => {
   const navOptions = (
     <>
@@ -30,15 +30,42 @@ const Navbar = () => {
             </ul>
           </div>
           <div className="flex flex-row items-center">
-        
-          <a className="btn btn-ghost text-xl"> <img src={logo} className = "h-8 rounded-md " alt="" />Skyview Apartments</a>
+            <a className="btn btn-ghost text-xl">
+              {" "}
+              <img src={logo} className="h-8 rounded-md " alt="" />
+              Skyview Apartments
+            </a>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{navOptions}</ul>
         </div>
+
+        
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <Link to = '/login'> <a className="btn">Login</a></Link>
+
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+              </div>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
