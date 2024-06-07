e1c5aeef8ebe744fd173952f38173b3434a5e863
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-07 09:41:05 +0600

    all apartment card shown in apartment page

--- src/Pages/Apartment/Apartment.jsx
+++ src/Pages/Apartment/Apartment.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
+import ApartmentCard from "./ApartmentCard";
 
 const Apartment = () => {
+  const [apartments, setApartments] = useState([]);
+  useEffect(() => {
+    fetch("apartments.json")
+      .then((res) => res.json())
+      .then((data) => setApartments(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <div>
       <Helmet>
         <title>Skyview Apartments | Apartment</title>
       </Helmet>
 
-      
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-20 lg:gap-y-32 mt-24">
+        {apartments.map((apartment) => (
+          <ApartmentCard key={apartment.apartment_no} apartment={apartment}></ApartmentCard>
+        ))}
+      </div>
     </div>
   );
 };
--- src/Pages/Apartment/ApartmentCard.jsx
+++ src/Pages/Apartment/ApartmentCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ApartmentCard = () => {
+const ApartmentCard = ({apartment}) => {
     return (
         <div>
             <div>
@@ -8,7 +8,7 @@ const ApartmentCard = () => {
           <div
             className="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md"
             style={{
-              backgroundImage: `url(https://images.unsplash.com/photo-1521903062400-b80f2cb8cb9d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80)`,
+              backgroundImage: `url(${apartment.apartment_image})`,
             }}
           />
           <div className="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
