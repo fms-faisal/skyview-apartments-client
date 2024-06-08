0dd4f51ec1a747c495b926c7fab1ad7954275065
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-09 05:21:15 +0600

    show progress bar while loading private route

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,13 +1,24 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../Providers/AuthProvider';
-import { Navigate } from 'react-router-dom';
+import React, { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
+import { Navigate } from "react-router-dom";
 
 const PrivateRoute = (children) => {
-    const {user} = useContext(AuthContext)
-    if (user) {
-        return children;
-    }
-    return <Navigate to ="/login"></Navigate>
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return (
+      <div>
+        <span className="loading loading-bars loading-xs"></span>
+        <span className="loading loading-bars loading-sm"></span>
+        <span className="loading loading-bars loading-md"></span>
+        <span className="loading loading-bars loading-lg"></span>
+      </div>
+    );
+  }
+  if (user) {
+    return children;
+  }
+  return <Navigate to="/login"></Navigate>;
 };
 
 export default PrivateRoute;
