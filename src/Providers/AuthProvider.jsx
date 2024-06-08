05d5378f5ba4e5bf71c7d3cf096034531059f3cc
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-08 06:17:04 +0600

    implemented onAuthStateChnaged   const [user, setUser] = useState(null)     const [loading, setLoading] = useState(true)
        useEffect(() => {
           const unsubscribe  = onAuthStateChanged(auth, currentUser => {
                setUser(currentUser);
                console.log((quote)currentUser(quote), currentUser)
                setLoading(false)
            });
            return () => {
                return unsubscribe;
            }
    
        }, [])

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,14 +1,33 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null)
+
+const auth = getAuth(app);
+
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+       const unsubscribe  = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('currentUser', currentUser)
+            setLoading(false)
+        });
+        return () => {
+            return unsubscribe;
+        }
+        
+    }, [])
+
+
     const authInfo  = {
         user, 
         loading,
-        
+
     }
     return (
         <AuthContext.Provider value = {authInfo}>
