fa0835e66f5be957d8c200271aad0895dc5d33c7
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-08 12:29:45 +0600

    react hook form implemented in signup page

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -4,8 +4,11 @@ import skyviewImage from "./../../../bannerImages/logo.jpeg";
 import googleLogo from "./../../../bannerImages/google-logo.svg";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProvider";
+import { useForm } from "react-hook-form";
 
 const Signup = () => {
+    const { register, handleSubmit,formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
     return (
         <div>
           <Helmet>
@@ -47,16 +50,16 @@ const Signup = () => {
     
                 <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
               </div>
-              <form>
+              <form onSubmit={handleSubmit(onSubmit)}>
               <div className="mt-4">
                   <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200" htmlFor="Name">
                     Name
                   </label>
                   <input
-                    id="Name"
+                    id="Name" {...register("name",  { required: true })}
                     className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
                     type="text" name = "name"
-                  />
+                  />  {errors.name && <span className="text-red-400">name is required*</span>}
                 </div>
                 <div className="mt-4">
                   <label className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200" htmlFor="LoggingEmailAddress">
@@ -65,8 +68,10 @@ const Signup = () => {
                   <input
                     id="LoggingEmailAddress"
                     className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
-                    type="email" name = "email"
+                    type="email" name = "email" {...register("email", { required: true })}
+                   
                   />
+                   {errors.email && <span className="text-red-400">email is required*</span>}
                 </div>
     
                 <div className="mt-4">
@@ -82,8 +87,9 @@ const Signup = () => {
                   <input
                     id="loggingPassword"
                     className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
-                    type="password" name = "password"
+                    type="password" name = "password" {...register("password", { required: true })}
                   />
+                   {errors.password && <span className="text-red-400">password is required*</span>}
                 </div>
     
                 <div className="mt-6">
