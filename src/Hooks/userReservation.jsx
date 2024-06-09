d52c40f4701c9532d43fe1cee10f28650a60555d
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-09 10:17:43 +0600

    tanstack query setup

---
+++ src/Hooks/userReservation.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const userReservation = () => {
+    
+};
+
+export default userReservation;
--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,20 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.jsx";
 import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from "./Providers/AuthProvider.jsx";
+import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <HelmetProvider>
-        <div className="w-[95%] mx-auto">
-          <RouterProvider router={router} />
-        </div>
-      </HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <div className="w-[95%] mx-auto">
+            <RouterProvider router={router} />
+          </div>
+        </HelmetProvider>
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>
 );
