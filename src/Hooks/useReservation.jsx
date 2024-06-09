8fb3e10e12eaf9b7131ca4a690c0c6ba57b92091
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-09 10:33:50 +0600

    added refetch and returned reservation adn refetch in array

--- src/Hooks/useReservation.jsx
+++ src/Hooks/useReservation.jsx
@@ -1,16 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useAxiosSecure, { axiosSecure } from './useAxiosSecure';
+import useAuth from './useAuth';
 
 const useReservation = () => {
     const axiosSecure = useAxiosSecure()
-    const {data: reservation = []} = useQuery({
-        queryKey: ['reservation'],
+    const {user} = useAuth()
+    const {refetch, data: reservation = []} = useQuery({
+        queryKey: ['reservation', user?.email],
         queryFn: async () => {
-            const response = await axiosSecure.get('/reservation')
+            const response = await axiosSecure.get(`/reservation?email=${user.email}`)
             return response.data
         }
     })
+    return [reservation, refetch]
 };
 
 export default useReservation;
