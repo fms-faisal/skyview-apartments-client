f5e2c0abad56760c66735bdf35e1a7263d0039f0
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-10 02:36:20 +0600

    all users table dynamic data and role and delete button created

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { FaTrashAlt, FaUsers } from "react-icons/fa";
 
 const AllUsers = () => {
   const axiosSecure = useAxiosSecure();
@@ -12,6 +13,14 @@ const AllUsers = () => {
       return res.data;
     },
   });
+
+  const handleDeleteUser = user => {
+
+  }
+
+  const handleRole = (user) => {
+    
+  }
   return (
     <div>
       <div className="flex justify-evenly my-4 ">
@@ -19,23 +28,33 @@ const AllUsers = () => {
         <h2 className="text-3xl">Total Users : {users.length}</h2>
       </div>
       <div className="overflow-x-auto">
-        <table className="table">
+        <table className="table w-full">
           {/* head */}
           <thead>
             <tr>
               <th></th>
               <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
+            {users.map((user, index) => (
               <tr key={user._id}>
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>Quality Control Specialist</td>
-                <td>Blue</td>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <button onClick={() => handleRole(user)}>
+                    <FaUsers className="text-green-400 mr-4" />
+                  </button>
+                </td>
+                <td>
+                  <button onClick={() => handleDeleteUser(user)}>
+                    <FaTrashAlt className="text-red-400 mr-4" />
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
