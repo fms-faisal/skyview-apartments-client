e62e238c9a5a732ae6cbda64db75c7724f58ae06
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-09 14:06:04 +0600

    user dashboard left side bar created

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,16 +1,35 @@
 import React from "react";
+import { FaBuilding, FaHome } from "react-icons/fa";
+import { FaCalendar, FaPaypal } from "react-icons/fa6";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
     <div className="flex">
-      <div className="w-64 min-h-full">
+      <div className="w-64 min-h-screen">
         <ul className="menu p-4">
           <li>
-            <NavLink to="/dashboard/userHome">User Home</NavLink>
+            <NavLink to="/dashboard/userHome">
+                <FaHome></FaHome> User Home</NavLink>
           </li>
           <li>
-            <NavLink to="/dashboard/reservation">My reservation</NavLink>
+            <NavLink to="/dashboard/reservation">
+                <FaCalendar></FaCalendar> My reservation</NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/paymentHistory">
+                <FaPaypal></FaPaypal> Payment History</NavLink>
+          </li>
+
+          <div className="divider"></div>
+
+          <li>
+            <NavLink to="/">
+            <FaHome></FaHome>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/apartment">
+            <FaBuilding></FaBuilding> Apartments</NavLink>
           </li>
         </ul>
       </div>
