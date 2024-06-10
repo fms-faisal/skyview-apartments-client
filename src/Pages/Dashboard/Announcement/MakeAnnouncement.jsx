dd7c05dba86f5fb6991953fd9693bad892d7a67a
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-10 10:18:45 +0600

    announcement is sent to database using axiosSecure and sweetalert is shown

--- src/Pages/Dashboard/Announcement/MakeAnnouncement.jsx
+++ src/Pages/Dashboard/Announcement/MakeAnnouncement.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 const MakeAnnouncement = () => {
+    const axiosSecure = useAxiosSecure();
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (data) => {
+    const announcement = {
+        title: data.title,
+        description: data.description
+    }
+
+    const announcementRes = await axiosSecure.post('/announcement', announcement);
+    console.log(announcementRes.data)
+
+    if (announcementRes.data.insertedId){
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Announcement posted successfully",
+            showConfirmButton: false,
+            timer: 1500
+          });
+    }
+
+
+  };
   return (
     <div className="flex justify-center items-center text-center h-full">
         <div className="flex flex-col justify-center items-center p-4 bg-gray-50 rounded-lg ">
