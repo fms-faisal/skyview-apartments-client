8a41f0531e9ca5d5528b3470f9385339a7f2353b
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-10 07:18:18 +0600

    useAdmin hook created and made dashboard conditional for user and admin

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const {logOut} = useAuth();
   axiosSecure.interceptors.request.use(function(config){
     const token = localStorage.getItem('access-token')
     console.log('request stopped by interceptors', token)
@@ -20,20 +24,15 @@ const useAxiosSecure = () => {
 
       axiosSecure.interceptors.response.use(function(response){
         return response;
-      }, (error) => {
+      }, async (error) => {
         const status = error.response.status;
         console.log('status error in the interceptor')
+        if (status === 401 || status === 403){
+          await logOut();
+          navigate('/login')
+        }
         return Promise.reject(error);
-      }
-
-
-
-
-
-
-
-
-
+      })
 
     
   return axiosSecure;
--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -2,44 +2,67 @@ import React from "react";
 import { FaBook, FaBuilding, FaHome, FaUser } from "react-icons/fa";
 import { FaCalendar, FaPaypal, FaPeopleGroup, FaPersonRifle } from "react-icons/fa6";
 import { NavLink, Outlet } from "react-router-dom";
+import useAdmin from "../Hooks/useAdmin";
 
 const Dashboard = () => {
-    const isAdmin = true;
+  const isAdmin = useAdmin();
   return (
     <div className="flex">
       <div className="w-56 min-h-screen flex items-center border-r-2 border-gray-200 rounded-lg">
         <ul className="menu p-4">
-          <li>
-            <NavLink to="/dashboard/profile">
-                <FaUser></FaUser> Profile</NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/reservation">
-                <FaCalendar></FaCalendar> My reservation</NavLink>
-          </li>
+          {isAdmin ? (
+            <>
+              <li>
+                <NavLink to="/dashboard/adminProfile">
+                  <FaUser></FaUser>Admin Profile
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink to="/dashboard/manageReservation">
+                  <FaBook></FaBook> Agreement Requests
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink to="/dashboard/allUsers">
+                  <FaUser></FaUser>Manage Members
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/dashboard/userProfile">
+                  <FaUser></FaUser>User Profile
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink to="/dashboard/reservation">
+                  <FaCalendar></FaCalendar> My reservation
+                </NavLink>
+              </li>
+            </>
+          )}
+
           <li>
             <NavLink to="/dashboard/paymentHistory">
-                <FaPaypal></FaPaypal> Payment History</NavLink>
+              <FaPaypal></FaPaypal> Payment History
+            </NavLink>
           </li>
-          <li>
-            <NavLink to="/dashboard/manageReservation">
-                <FaBook></FaBook> Agreement Requests</NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/allUsers">
-                <FaUser></FaUser>Manage Members</NavLink>
-          </li>
-
 
           <div className="divider"></div>
 
           <li>
             <NavLink to="/">
-            <FaHome></FaHome>Home</NavLink>
+              <FaHome></FaHome>Home
+            </NavLink>
           </li>
           <li>
             <NavLink to="/apartment">
-            <FaBuilding></FaBuilding> Apartments</NavLink>
+              <FaBuilding></FaBuilding> Apartments
+            </NavLink>
           </li>
         </ul>
       </div>
